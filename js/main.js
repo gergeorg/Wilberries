d4cdd1defb07d22067abf2cace90311cc02bc6f0
gergeorg <62167850+gergeorg@users.noreply.github.com>
2021-03-26 17:03:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,31 +7,150 @@ const mySwiper = new Swiper('.swiper-container', {
 });
 
 
-//cart
 
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
 const modalClose = document.querySelector('.modal-close');
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
 
-const openModal = function () {
+const getGoods = async () => {
+  const result = await fetch('db/db.json');
+  if (!result.ok) {
+    throw 'Упс! Ошибочка вышла: ' + result.status
+  }
+  return await result.json();
+};
+
+//cart
+
+const cart = {
+  cartGoods: [],
+
+  renderCart(){
+    cartTableGoods.textContent = '';
+    this.cartGoods.forEach(({ id, name, price, count }) => {
+      const trGood = document.createElement('tr');
+      trGood.className = 'cart-item';
+      trGood.dataset.id = id;
+
+      trGood.innerHTML = `
+                          <td>${name}</td>
+                          <td>${price}$</td>
+                          <td><button class="cart-btn-minus">-</button></td>
+                          <td>${count}</td>
+                          <td><button class="cart-btn-plus">+</button></td>
+                          <td>${price * count}$</td>
+                          <td><button class="cart-btn-delete">x</button></td>
+      `;
+      cartTableGoods.append(trGood);
+    });
+
+    const totalPrice = this.cartGoods.reduce((sum, item) => {
+      return sum + item.price * item.count;
+    }, 0);
+
+    cardTableTotal.textContent = totalPrice + '$'
+  },
+
+  deleteGood(id){
+    this.cartGoods = this.cartGoods.filter(item => id !== item.id);
+    this.renderCart();
+  },
+
+  minusGood(id){
+    for (const item of this.cartGoods) {
+      if (item.id === id) {
+        if (item.count <= 1) {
+          this.deleteGood(id)
+        } else {
+          item.count--;
+        }
+        break;
+      }
+    }
+    this.renderCart();
+  },
+
+  plusGood(id){
+    for (const item of this.cartGoods) {
+      if (item.id === id) {
+        item.count++;
+        break;
+      }
+    }
+    this.renderCart();
+  },
+
+  addCartGoods(id){
+    const goodItem = this.cartGoods.find(item => item.id === id);
+    if (goodItem) {
+      this.plusGood(id);
+    } else {
+      getGoods()
+        .then(data => data.find(item => item.id === id))
+        .then(({id, name, price}) => {
+          this.cartGoods.push({
+            id,
+            name,
+            price,
+            count: 1
+          });
+        });
+    }
+  },
+}
+
+document.body.addEventListener('click', event => {
+  const addToCart = event.target.closest('.add-to-cart');
+
+  if (addToCart) {
+    cart.addCartGoods(addToCart.dataset.id);
+  }
+})
+
+cartTableGoods.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.tagName === 'BUTTON') {
+    const id = target.closest('.cart-item').dataset.id;
+
+    if (target.classList.contains('cart-btn-delete')) {
+      cart.deleteGood(id);
+    };
+
+    if (target.classList.contains('cart-btn-minus')) {
+      cart.minusGood(id);
+    }
+
+    if (target.classList.contains('cart-btn-plus')) {
+      cart.plusGood(id);
+    }
+  }
+})
+
+const openModal = () => {
+  cart.renderCart();
 	modalCart.classList.add('show');
 }
 
-const closeModal = function () {
+const closeModal = () => {
 	modalCart.classList.remove('show');
 }
 
 buttonCart.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
 
-
 //scroll smooth
 
 {
-	const scrollLinks = document.querySelectorAll('a.scroll-link')
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
 	for (const scrollLink of scrollLinks) {
-		scrollLink.addEventListener('click', (e) => {
+		scrollLink.addEventListener('click', e => {
 			e.preventDefault();
 			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
@@ -44,25 +163,12 @@ modalClose.addEventListener('click', closeModal);
 
 //goods
 
-const more = document.querySelector('.more')
-const navigationLink = document.querySelectorAll('.navigation-link')
-const longGoodsList = document.querySelector('.long-goods-list')
-
-const getGoods = async function () {
-  const result = await fetch('db/db.json');
-  if (!result.ok) {
-    throw 'Упс! Ошибочка вышла: ' + result.status
-  }
-  return await result.json();
-};
-
 const createCard = function (objCard) {
   const card = document.createElement('div');
   card.className = 'col-lg-3 col-sm-6'
   card.innerHTML = `
                     <div class="goods-card">
                       ${objCard.label ? `<span class="label">${objCard.label}</span>` : ''}
-
                       <img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
                       <h3 class="goods-title">${objCard.name}</h3>
                       <p class="goods-description">${objCard.description}</p>
@@ -78,7 +184,7 @@ const createCard = function (objCard) {
 const renderCard = function(data) {
   longGoodsList.textContent = '';
   const cards = data.map(createCard)
-  longGoodsList.append(...cards)
+  longGoodsList.append(...cards) 	// ... - spread, распаковывает массив в отдельные элементы
     document.body.classList.add('show-goods')
 };
 
@@ -91,18 +197,12 @@ more.addEventListener('click', (e) => {
 
 const filterCards = function(field, value) {
   getGoods()
-  .then(function(data) {
-    const filteredGoods = data.filter(function(good) {
-      return good[field] === value
-    });
-    return filteredGoods;
-  })
-
+  .then(data => data.filter(good => good[field] === value))
   .then(renderCard);
 };
 
 navigationLink.forEach((link) => {
-  link.addEventListener('click', (event) => {
+  link.addEventListener('click', event => {
     event.preventDefault();
     const field = link.dataset.field;
     const value = link.textContent;
